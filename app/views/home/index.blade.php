f94ba401fc67237186919f298af2d9aa68f22972
Jeff Mills <jeffrey.mills@outlook.com>
2014-11-21 21:53:27 -0600

    Random Color Palette Generator
    Generate color palettes and stuff.

---
+++ app/views/home/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.master')
+
+@section('content')
+<section>
+</section>
+<footer>
+	<p>[ Press spacebar to load a new palette ]</p>
+</footer>
+@stop
---
+++ public/js/main.js
@@ -0,0 +1,44 @@
+$(function() {
+
+	var url = 'http://www.colourlovers.com/api/palettes?numResults=100&format=json&jsonCallback=?';
+	var colorArray = [];
+	var startNum = 0;
+
+	function getPalettes() {
+		$.getJSON(url, function(data) {
+			$.each(data, function(k,v) {
+				colorArray.push(v.colors);
+			});
+		}).then(loadPalette);
+	}
+
+	function cyclePalette() {
+		$(window).on('keypress', function(e) {
+			if(e.keyCode === 32) {
+				if(startNum === colorArray.length) {
+					startNum = 0;
+				}
+				$('section').empty();
+				loadPalette();				
+			}
+		})
+	}
+
+	function loadPalette() {
+		$.each(colorArray[startNum], function(k,v) {
+			$('<div/>',{
+				class: 'color',
+				'data-hex': v,
+				html: '<p>#'+v+'</p>',
+			}).css('background','#' + v).appendTo('section');
+		});
+		startNum = startNum + 1;
+		console.log(colorArray[startNum]);
+	}
+
+	function init() {
+		getPalettes();
+		cyclePalette();
+	}
+	init();
+});

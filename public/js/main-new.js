5aefd1f1ca68e2e7dcd24ffa4e0e00e9ed0a681f
Jeff Mills <jeffrey.mills@outlook.com>
2015-02-04 21:47:30 -0600

    adding ability to lock hex values

--- public/js/main-new.js
+++ public/js/main-new.js
@@ -12,22 +12,30 @@ $(function() {
 			var format = '?format=json';
 			var jsonCallback = '&jsonCallback=?';
 			var hex_logic = '&hex_logic=AND';
-			var hex = '&hex='+arg;
+			var hex = '&hex=' + arg;
 			var numResults = 50;
-			var compiledUrl = baseUrl+category+format+jsonCallback+hex_logic+hex+numResults;
+			var compiledUrl = baseUrl+				
+				category+
+				format+
+				jsonCallback+
+				hex_logic+
+				hex+
+				'&numResults='+numResults;
 			return compiledUrl;
 		},
+
 		loadPalettesOnScreen: function() {
 			$.each(this.paletteArray[this.startNumber], function(k,v) {
 				$('<div/>', {
 					class: 'color',
 					'data-hex' : v,
-					html: '<p>#'+v+' <span>(Locked)</span></p>',
+					html: '<p>#' + v + ' <span>(Locked)</span></p>',
 				}).css({
-					'background' : '#'+v
+					'background' : '#' + v
 				}).appendTo('section');
 			});
 		},
+
 		cyclePalette: function() {
 			var $this = this;
 			$(window).on('keypress', function(e) {
@@ -36,15 +44,28 @@ $(function() {
 					if($this.startNumber === $this.paletteArray.length) {
 						$this.startNumber = 0;
 					}
+					console.log($this.startNumber);
 					$('section').empty();
-					//$this.getPaletteData();
 					$this.loadPalettesOnScreen();
 				}
 			});
 		},
+
+		lockValue: function() {
+			var $this = this;
+			$(document).on('click','.color', function() {
+				$this.lockedValues.push( $(this).data('hex') );
+				$this.getPaletteData();
+				$this.cyclePalette();
+			})
+		},
+
 		getPaletteData: function() {
 			var $this = this;
+			$this.paletteArray.length = 0;
 			$.getJSON( $this.compileUrl($this.lockedValues,'top'), function(data) {
+				console.log( $this.compileUrl($this.lockedValues,'top') );
+				console.log(data.length);
 				$.each(data, function(key,value) {
 					$this.paletteArray.push(value.colors);
 				});
@@ -52,9 +73,9 @@ $(function() {
 				$('section').empty();				
 				$this.loadPalettesOnScreen();
 			});
-		},
-	}
-	
+		}
+	}	
 	App.getPaletteData();
 	App.cyclePalette();
+	App.lockValue();
 });
